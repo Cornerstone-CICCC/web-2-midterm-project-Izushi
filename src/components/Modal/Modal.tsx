953c5d1f577529086ff6eb3021feb1e0d56a5059
Daz <shoeshinesamurai@gmail.com>
2024-12-03 02:01:03 -0800

    Created Modal components

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Modal.css';
+
+interface ModalProps {
+  movie: any;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Modal: React.FC<ModalProps> = ({ movie, isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button
+          className="modal-close"
+          onClick={onClose}
+        >Close</button>
+        <h2>{movie.title}</h2>
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
+        <p>{movie.overview}</p>
+        <p>Release Date: {movie.release_date}</p>
+        <p>Rating: {movie.vote_average}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Modal
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import Modal from '../../components/Modal/Modal';
 
 interface Movies {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  backdrop_path: string;
+  vote_average: number;
+  release_date: string;
+}
+
+interface Movie {
   id: number;
   title: string;
   overview: string;
@@ -15,11 +27,42 @@ interface HomeProps {
 }
 
 const Home: React.FC<HomeProps> = ({ movies }) => {
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const aipKey = "0a8d1904066c27fb5552becee7441627";
+  const detailEndPoint = "https://api.themoviedb.org/3/movie/";
+
+  const fetchMovieDetails = (id: number) => {
+    axios
+      .get(`${detailEndPoint}${id}?api_key=${aipKey}`)
+      .then((res) => {
+        const result = res.data;
+        console.log(result);
+        setSelectedMovie(result);
+        setIsModalOpen(true);
+      })
+      .catch(err => console.error(err));
+  }
+
+  const handleMovieClick = (id: number) => {
+    fetchMovieDetails(id);
+  }
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedMovie(null);
+  }
+
   return (
     <div className="movies">
       {movies.map((movie) => {
         return (
-          <div key={movie.id} className="movie">
+          <div
+            key={movie.id}
+            className="movie"
+            onClick={() => handleMovieClick(movie.id)}
+          >
             <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title} />
             <div className="movie-info">
               <h3>{movie.title}</h3>
@@ -28,6 +71,9 @@ const Home: React.FC<HomeProps> = ({ movies }) => {
           </div>
         )
       })}
+      {selectedMovie && (
+        <Modal movie={selectedMovie} onClose={closeModal} isOpen={isModalOpen} />
+      )}
     </div>
   )
 }
